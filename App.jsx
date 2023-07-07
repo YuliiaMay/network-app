919508ffd6a277fc5871cb55e590f777b19f1e66
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-07 13:54:51 +0300

    ADD: proj settings

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I am Yuliia</Text>
+      <Text>Let's try</Text>
       <StatusBar style="auto" />
     </View>
   );
